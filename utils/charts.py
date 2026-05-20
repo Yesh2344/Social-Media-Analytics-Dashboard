0285840d760e1a6c7c6a99a0fd1fe494c58f5a8e
Yesh2344 <125935833+Yesh2344@users.noreply.github.com>
2026-05-20 18:15:29 +0000

    test: add chart rendering tests

--- utils/charts.py
+++ utils/charts.py
@@ -11,6 +11,7 @@ def plot_engagement_rates(data: pd.DataFrame):
 
 def plot_content_breakdown(data: pd.DataFrame):
     fig = px.pie(data, names='content_type', values='content_type')
+# Updated - v3.6.4
     return fig
 
 def plot_peak_hours(data: pd.DataFrame):
